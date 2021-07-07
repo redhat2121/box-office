8520a6e0f85d41886599b22d67ce1112b699f004
redhat2121 <redhat2121@protonmail.com>
2021-07-07 08:44:15 +0530

    Updates

---
+++ precache-manifest.aee086c3f2ea441976ff4a3c0424ad90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e2ff13c9a3abc5fcf1c9672d2255ed",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "fe0b416847d19c592674",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "2d7cac9f23335dcd0937",
+    "url": "/box-office/static/js/2.aa7d3fe6.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/box-office/static/js/2.aa7d3fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0b416847d19c592674",
+    "url": "/box-office/static/js/main.f8906d87.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  },
+  {
+    "revision": "75bd07e4ae643cf1305c70032e8fb1f8",
+    "url": "/box-office/static/media/mustang.75bd07e4.jpg"
+  }
+]);
